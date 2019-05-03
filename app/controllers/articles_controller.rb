d1fbb0ae504b8908478f9c84776e1f8e759a4c3f
gregwegrzyn88 <gregwegrzyn88@gmail.com>
2019-05-03 13:58:17 -0500

    extract redncancy and prepare for production

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -10,7 +10,6 @@ class ArticlesController < ApplicationController
   end
 
   def edit
-    @article = Article.find(params[:id])
     
   end
 
@@ -25,7 +24,6 @@ class ArticlesController < ApplicationController
   end
 
   def update
-    @article = Article.find(params[:id])
     if @article.update(article_params)
       flash[:notice] = "Article was successfully updated"
       redirect_to article_path(@article)
@@ -35,11 +33,9 @@ class ArticlesController < ApplicationController
   end
 
   def show
-    @article = Article.find(params[:id])
   end
 
   def destroy
-    @article = Article.find(params[:id])
     @article.destroy
     flash[:notice] = "Article Was Deleted"
     redirect_to articles_path
@@ -52,6 +48,10 @@ class ArticlesController < ApplicationController
     end
 
     def set_article
+    end
+
+    def set_article
+      @article = Article.find(params[:id])
     end
   
 end
